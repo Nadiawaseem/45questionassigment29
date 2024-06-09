9b902e3d14cb4dad3f34198ede56b046d80f2a16
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 17:12:40 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,22 @@
+   let favourite_fruits = ["Mango","Banana","Apple"];
+
+
+   if ( favourite_fruits.includes ("Mango")) {
+    console.log("you really like mango!");
+
+   }
+   if (favourite_fruits.includes ("Apple")) {
+    console.log("you really like Apple!");
+   
+    
+   }
+    if ( favourite_fruits.includes ("Banana")) {
+        console.log("you really like Banana!");
+    }
+        if ( favourite_fruits.includes ("Stawberry")) {
+            console.log("you really like Stawberry!");
+        }
+            if ( favourite_fruits.includes ("Orange")) {
+               console.log("you really like Orange!");
+            }
+                
